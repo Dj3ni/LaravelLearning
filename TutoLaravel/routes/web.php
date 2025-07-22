dbf8eed11d736dc0a1b15dea1e723c7a168b3bef
Dj3ni <dj3n.dev@gmail.com>
2025-07-22 17:22:41 +0200

    tuto routing

---
+++ TutoLaravel/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function (): View {
+    return view('welcome');
+});
+
+Route::prefix('/blog')->name('blog.')->group(function (){    
+	Route::get('/',function (){            
+		return [            
+			"link" => \route('show', ["slug" =>"article", "id"=>12])        
+		];    
+	})    
+	->name('index');
+	
+    Route::get('/{slug}-{id}', function(string $slug, string $id) {            
+        return [                
+            "slug" => $slug,                
+            "id" => $id            
+		];    
+	})    
+	->where([        
+		"id" => '[0-9]+',        
+		"slug" => '[a-z0-9\-]+'     
+	])    
+	->name('show');
+});
